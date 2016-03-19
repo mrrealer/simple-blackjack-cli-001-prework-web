f6441d7e5da674d2a3d5c11e6135b99f685db46f
mrrealer <dwayne.samuel@gmail.com>
2016-03-19 05:21:05 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,64 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  return card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    return puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
+
+#This method represents the first round of the game for a given player. It should call on the #deal_card method twice, use the #display_card_total method to puts out the sum and then return the sum. This method will therefore call on two other helper methods, #deal_card and #display_card_total, which takes in an argument of the sum of both invocations of #deal_card.
 def initial_round
-  # code #initial_round here
+  sum = deal_card + deal_card
+  return display_card_total(sum)
 end
 
-def hit?
-  # code hit? here
+def hit?(initial_round)
+  prompt_user
+  get_user_input
+  return initial_round 
+  if input == "h" do
+    deal_card = deal_card + initial round
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  # code runner here
-end
+# def runner
+#   welcome
+#   initial_round
+#   hit?(initial_round)
+#   display_card_total(initial_round)
+#   until 
+#     card_total > 21
+#   end
+#   end_game
+# end
+
+# runner
     
